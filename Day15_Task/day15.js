b400a6c1f5073791c6b2fd5e6585f70da23216a0
Anbuvel2005 <cc7165571655022@gmail.com>
2026-07-13 22:13:37 +0530

    day15

---
+++ Day15_Task/day15.js
@@ -0,0 +1,48 @@
+// Log your name and age in console.
+
+// let name = "Anbuvel";
+// let age = 21;
+
+// console.log(name);
+// console.log(age);
+
+// Write single-line and multi-line comments.
+
+// let name = "Anbuvel";
+// let age = 21;
+
+/*console.log(name);
+console.log(age);*/
+
+// Declare variables using var, let, and const.
+
+// let a = 10;
+// const age = 20;
+// var food = "parotta";
+
+// console.log(10);
+// console.log(20);
+// console.log(food);
+
+// Create variables with string, number, boolean, null, undefined.
+
+// String
+let name = "Anbu";
+
+// Number
+let age = 21;
+
+// Boolean
+let isStudent = true;
+
+// Null
+let address = null;
+
+// Undefined
+let phoneNumber;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(address);
+console.log(phoneNumber);

7f6ccba6be64a2b81a364be53f9ee0e20e97d386
Anbuvel2005 <cc7165571655022@gmail.com>
2026-07-17 16:50:22 +0530

    day17

---
+++ Day17/conditions.js
@@ -0,0 +1,101 @@
+// // Use +, -, *, /, %, ==, ===, &&, ||.
+
+// //+
+// // let a = 10;
+// // let b = 20;
+
+// // let c = a + b;
+// // console.log(c);
+
+// // -
+
+// // let a = 10;
+// // let b = 20;
+
+// // let c = a - b;
+// // console.log(c);
+
+// //*
+
+// // let a = 10;
+// // let b = 20;
+
+// // let c = a * b;
+// // console.log(c);
+
+// //  /
+
+// // let a = 10;
+// // let b = 20;
+
+// // let c = a / b;
+// // console.log(c);
+
+// // %
+
+// // let a = 10;
+// // let b = 20;
+
+// // let c = a % b;
+// // console.log(c);
+
+// //==
+
+// let a = 10;
+// let b = "10";
+
+// console.log(a == b);
+
+// //===
+
+// // let a = 10;
+// // let b = 20;
+
+// // console.log(a === b);
+
+// Check if a number is positive or negative.
+
+// let num = 0;
+
+// if (num > 0) {
+//   console.log("positive");
+// } else {
+//   console.log("negative");
+// }
+
+// Create a switch to print days of the week.
+
+let n = "monday";
+
+switch (n) {
+  case "monday":
+    console.log("Monday");
+    break;
+
+  case "tuesday":
+    console.log("Tuesday");
+    break;
+
+  case "wednesday":
+    console.log("Wednesday");
+    break;
+
+  case "thursday":
+    console.log("Thursday");
+    break;
+
+  case "friday":
+    console.log("Friday");
+    break;
+
+  case "saturday":
+    console.log("Saturday");
+    break;
+
+  case "sunday":
+    console.log("Sunday");
+    break;
+
+  default:
+    console.log("Invalid day");
+}

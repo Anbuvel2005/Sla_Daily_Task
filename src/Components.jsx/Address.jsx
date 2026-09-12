8a6e3f848f68022ff7a9f14856b1eb18b300ae72
Anbuvel2005 <cc7165571655022@gmail.com>
2026-09-12 19:27:22 +0530

    day1_React

---
+++ src/Components.jsx/Address.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Address() {
+  return (
+    <div>
+      <h1>Address details</h1>
+      <p>2/309</p>
+      <p>udayarpalayam taluk</p>
+      <p>Ariyalur</p>
+      <p>612903</p>
+    </div>
+  );
+}
---
+++ src/Components.jsx/Product.jsx
@@ -0,0 +1,10 @@
+export default function Product() {
+  return (
+    <div>
+      <h1>Product details</h1>
+      <p>Product_name:Laptop</p>
+      <p>Product_rice:2000000</p>
+      <p></p>
+    </div>
+  );
+}
